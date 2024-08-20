b06aa453c534d5dd4b698af5ce16a8a5176a89dd
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-20 16:59:29 +0700

    feat: advice generator app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)',
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
       },
     },
   },
